a669cf9dbcf1a78adb89cda8360b518e4502b42e
Garrett Dees <garrettdees@gmail.com>
2020-07-19 14:57:34 -0500

    Updated killes.sh with new code from the multi_switch.sh shutdown script

--- lib/killes.sh
+++ lib/killes.sh
@@ -5,17 +5,15 @@
 # Check if EmulationStation is running. Finish the script if isn't.
 espid="$(pgrep -f "/opt/retropie/supplementary/.*/emulationstation([^.]|$)")" || exit 0
 
-# The "sed" command below isn't a crypted message :)
-# It's just a trick to make $emucall regex-safe to use in the "pgrep -f" below.
-emucall="$(sed '4!d; s/\([\\"]\|[[:alnum:]_]\+=[^ ]* \)//g; s/[][(){}^$*.|+? ]/\\&/g' /dev/shm/runcommand.info)"
-
 # If there's an emulator running, we need to kill it and go back to ES.
-if [[ -n "$emucall" ]]; then
-    emupid="$(pgrep -f "$emucall" | tr '\n' ' ')"
+emupid="$(pgrep -f -n runcommand.sh)"
+if [[ -n "$emupid" ]]; then
     pkill -P "$(echo $emupid | tr ' ' ',')"
     kill "$emupid"
-    wait "$emupid"
-    sleep 5 # maybe it can be less
+    # Hold in this while loop until the emulator process stops
+    while s=$(ps -p "$emupid" -o s=) && [ "$s" ] && [ "$s" != 'Z' ]; do
+        sleep 1
+        done
 fi
 
 kill "$espid"
